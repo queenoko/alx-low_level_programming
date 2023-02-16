b793294cfdf4216f75aaba1003d3c1f74788773f
queenoko <quimaudongwo@gmail.com>
2023-02-16 10:13:12 -0800

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Program thta prints exactly the proper grammer
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
