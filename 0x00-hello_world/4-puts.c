bf1d3db9afcde1b4d5dadf62f0ed0f82721b9141
queenoko <quimaudongwo@gmail.com>
2023-02-16 07:34:17 -0800

    Hello, Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * Write a program that prints PROGRAMMING IS LIKE BUILDING A MULTILINGUAL PUZZLE
+ * Return: 0 (succecc)
+ */
+
+int main(void)
+{
+puts("\n"Programming is like building a multilingual puzzle");
+return (0);
+}
+
+
