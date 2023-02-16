cad4299b1e49e95dae880f01271c9b0dfa9e5aae
queenoko <quimaudongwo@gmail.com>
2023-02-16 12:33:45 -0800

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Program that prints exactly the proper grammer
+ * main - Program that prints exactly the proper grammer function
  * Return: 0 (Success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece
-of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n")
 return (0);
 }
+
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Program that prints various types of datatypes"
+ * Return 0 (success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+printf("Size of a char: %lu bytes(s)\n", (unsigned long)Sizeof(a));
