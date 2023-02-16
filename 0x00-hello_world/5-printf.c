ea31cc65a16b235fbbfddc87080781af456da1e8
queenoko <quimaudongwo@gmail.com>
2023-02-16 15:19:12 -0800

    printf4

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Program that prints exactly the proper grammer function
+ * main - Program that prints exactly the proper grammer
  * Return: 0 (Success)
  */
 int main(void)
@@ -8,4 +8,3 @@ int main(void)
 printf("with proper grammar, but the outcome is a piece of art,\n")
 return (0);
 }
-
