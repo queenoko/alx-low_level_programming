422a0d9b9bbcba46f9c4641fc0ce7eca5574c1d2
queenoko <quimaudongwo@gmail.com>
2023-02-17 13:21:05 -0800

    Combinations of three digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,8 +13,8 @@ int main(void)
 	{
 		for (j = i + 1; j < 10; j++)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
 
 			if (i == 8 && j == 9)
 			{
