db916846bc51dc81e9c5c59698bdb582b87cfef4
queenoko <quimaudongwo@gmail.com>
2023-02-17 07:24:31 -0800

    Base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char b;
+
+	int x;
+
+	b = 'a';
+	while (x < 10)
+	{
+		putchar(x + '0');
+		x++
+	}
+	while (b <= 'f')
+	{
+		putchar(b);
+		b++;
+	}
+	putchar('\n');
+	return (0);
+}
