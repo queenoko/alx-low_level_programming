7cfb4336cbe69b1a1c4f794abeaec6d8485e5c58
queenoko <quimaudongwo@gmail.com>
2023-02-17 06:30:16 -0800

    Single Digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints single digit of numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		putchar("%d", a);
+	putchar('\n');
+	return (0);
+}
