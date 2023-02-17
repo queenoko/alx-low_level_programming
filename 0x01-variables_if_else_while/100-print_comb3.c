929abae549e771e136f1b7b8227d6aaec11b5d9e
queenoko <quimaudongwo@gmail.com>
2023-02-17 12:00:19 -0800

    Combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,12 +13,12 @@ int main(void)
 	{
 		for (j = i + 1; j < 10; j++)
 		{
-			putchar((i % 10) + '0');
-			putchar((j % 10) + '0');
+			putchar(i + '0');
+			putchar(j + '0');
 
 			if (i == 8 && j == 9)
 			{
-				putchar(',' ',');
+				putchar(',');
 				putchar(' ');
 			}
 		}
