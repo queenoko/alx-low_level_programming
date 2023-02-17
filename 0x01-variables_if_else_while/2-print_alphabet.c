04e1d34a302843752a65c4acc1a44c48afc93e4a
queenoko <quimaudongwo@gmail.com>
2023-02-17 05:24:25 -0800

    Alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Print alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char m;
+
+	m = 'a';
+	while (c <= 'z')
+	{
+		putchar(m);
+		m++;
+	}
+	putchar('\n');
+	return (0);
