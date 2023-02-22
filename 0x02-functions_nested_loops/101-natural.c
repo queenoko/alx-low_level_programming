38da6f184f4ab28dbbdeab7c39d036054ec862d7
queenoko <quimaudongwo@gmail.com>
2023-02-21 17:14:14 -0800

    Multiples of 3

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints natural numbers below 10 that are multiples of 3
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	int x, y = 0;
+
+	while (x < 1024)
+	{
+		if ((x % 3 == 0) || (i % 5 == 0))
+		{
+			y += x;
+		}
+		i++;
+	}
+	printf("%d\n", y);
+	return (0);
+}
