ff0f557db389e0a79bc5df323bc2ca33db54d78f
queenoko <quimaudongwo@gmail.com>
2023-02-21 18:00:58 -0800

    Time Table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,18 +4,13 @@
  * @n: input number
  * Return: matrix
  */
-
 void print_times_table(int n)
 {
 	int a, b, c;
-
 	if (n >= 0 && n <= 14)
-	{
-		for (a = 0; a <= n; a++)
-		{
-			for (b = 0; b <= n; b++)
-			{
-				c = a * b;
+	{for (a = 0; a <= n; a++)
+		{for (b = 0; b <= n; b++)
+			{c = a * b;
 				if (c > 99)
 				{
 					_putchar(',');
@@ -23,26 +18,21 @@ void print_times_table(int n)
 					_putchar((c / 100) + '0');
 					_putchar(((c / 10) % 10) + '0');
 					_putchar((c % 10) + '0');
-				}
-				else if (c > 9)
+				}else if (c > 9)
 				{
 					_putchar(',');
 					_putchar(32);
 					_putchar(32);
 					_putchar(((c / 10) % 10) + '0');
 					_putchar((c % 10) + '0');
-				}
-
-				else
-				{
-					if (b != 0)
+				}else
+				{if (b != 0)
 					{
 						_putchar(',');
 						_putchar(32);
 						_putchar(32);
 						_putchar(32);
 					}
-
 					_putchar(c + '0');
 				}
 			}
