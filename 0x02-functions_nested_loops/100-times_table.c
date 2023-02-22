cd3765e33c728ed522b8f25bdb705014bc905123
queenoko <quimaudongwo@gmail.com>
2023-02-21 18:38:56 -0800

    Times Table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,6 +7,7 @@
 void print_times_table(int n)
 {
 	int a, b, c;
+
 	if (n >= 0 && n <= 15)
 	{
 		for (a = 0; a <= n; a++)
@@ -19,13 +20,14 @@ void print_times_table(int n)
 				c = a * b;
 				if (c <= 99)
 					_putchar(' ');
-				if(c <= 9)
+				if (c <= 9)
 					_putchar(' ');
 				if (c >= 100)
 				{
 					_putchar((c / 100) + '0');
 					_putchar(((c / 10)) % 10 + '0');
-				}else if (c <= 99 && c >= 10)
+				}
+				else if (c <= 99 && c >= 10)
 				{
 					_putchar((c / 10) + '0');
 				}
