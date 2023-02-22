1ba1418efab57b076a40dabe1175db7df6cee360
queenoko <quimaudongwo@gmail.com>
2023-02-22 03:33:43 -0800

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Test for postive or negative number function
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	a = 0;
+	positive_or_negative(a);
+
+	return (0);
+}
