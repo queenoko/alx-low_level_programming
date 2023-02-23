c3fde85d378ab94600621a74eb2f78813d6d3aad
queenoko <quimaudongwo@gmail.com>
2023-02-23 02:13:26 -0800

    Multiply two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+/*
+ * File: 2-mul.c
+ * Author: Queensly Udongwo
+ */
+
+#include "main.h"
+
+/**
+ * mul - Multiply two integers.
+ * @a: first integer input
+ * @b: second integer input
+ *
+ * Return: Result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
