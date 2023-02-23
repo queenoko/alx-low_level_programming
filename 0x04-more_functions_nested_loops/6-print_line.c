00dc9f73328aed179da10d50a192c7efa6b45933
queenoko <quimaudongwo@gmail.com>
2023-02-23 03:13:10 -0800

    straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+/*
+ * File: 6-print_line.c
+ * Author: Queensly Udongwo
+ */
+
+#include "main.h"
+
+/**
+ * print_line - Draw a straight line
+ * @n: number of straight line
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int lin;
+
+	if (n > 0)
+	{
+		for (lin = 0; lin < n; lin++)
+			_putchar('_');
+	}
+	_putchar('\n');
+	return (0);
+}
