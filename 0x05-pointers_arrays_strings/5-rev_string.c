14806630ba0a484b51cd7722f0859ed1022a8b89
queenoko <quimaudongwo@gmail.com>
2023-02-27 09:46:59 -0800

    reverse1

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,3 +3,24 @@
  * Author: Queensly Udongwo
  */
 
+#include "main.h"
+
+/**
+ * rev_string - Reverse a string
+ * @s: string reversed
+ */
+void rev_string(char *s)
+{
+	int m = 0, index = 0;
+	char temp;
+
+	while (s[index++])
+		m++;
+
+	for (index = m - 1; index >= m / 2; index--)
+	{
+		temp = s[index];
+		s[index] = s[m - index - 1];
+		s[m - index - 1] = temp;
+	}
+}
