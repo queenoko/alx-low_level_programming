ad1f01334b47d4606fda2d99201c26ecef462cac
queenoko <quimaudongwo@gmail.com>
2023-02-27 08:01:06 -0800

    String in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+/*
+ * File: 4-print_rev.c
+ * Author: Queensly Udongwo
+ */
+
+#include "main.h"
+
+/**
+ * print_rev -  prints a string, in reverse
+ * @s: Input string
+ * l is length i is index
+ */
+
+void print_rev(char *s)
+{
+	int l = 0, i;
+
+	while (s[i++])
+		l++;
+
+	for (i = l - 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+
+}
