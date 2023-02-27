382edff24c10a3d664cdbd74685b80eb77112c2e
queenoko <quimaudongwo@gmail.com>
2023-02-27 07:02:15 -0800

    pointer to Int

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+/*
+ * File: 0-reset_to_98.c
+ * Author: Queensly Udongwo
+ */
+
+#include "main.h"
+/**
+ * reset_to_98 - Function that takes a pointer to an int
+ * @n: Input
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
