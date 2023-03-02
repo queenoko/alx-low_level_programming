ab7cb8245aaad6327bc6a33c26335908992c6ea2
queenoko <quimaudongwo@gmail.com>
2023-03-01 18:37:40 -0800

    Adds two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -24,7 +24,22 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 
 	for (; *n1 && *n2; n1--, n2--, r_index--)
 	{
+		num = (*n1 - '0') + (*n2 - '0');
+		num += tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	for (; *n1; n1--, r_index--)
+	{
 		num = (*n1 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	for (; *n2; n2--, r_index--)
+	{
+		num = (*n2 - '0') + tens;
 		*(r + r_index) = (num % 10) + '0';
 		tens = num / 10;
 	}
