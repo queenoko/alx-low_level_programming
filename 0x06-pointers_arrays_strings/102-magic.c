5ced6b4f0df82e6033cdc7eed404e938f164b258
queenoko <quimaudongwo@gmail.com>
2023-03-01 17:27:30 -0800

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ * main - Print new line
+ * Return: 0
+ */
 int main(void)
 {
 	int n;
@@ -10,11 +13,8 @@ int main(void)
 	p = &n;
 	/*
 	 * write your line of  code here...
-	 * Remember:
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
-	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
 	 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n*/
---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,8 @@
+/*
+ * File: 103-infinite_add.c
+ * Author: Queensly Udongwo
+ */
+
+#include "main.h"
+
+char *add_strings(char *n1, char *n2, char *r, int r_index
