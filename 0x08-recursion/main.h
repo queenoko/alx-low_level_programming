bf85387747c982e9adc7d2b3499ed71ea9f5cfca
queenoko <quimaudongwo@gmail.com>
2023-03-08 07:52:40 -0800

    Print a string

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Authour: Queensly Udongwo
+ * Description: Header file containing prototypes for all functions
+ *              used in 0x08-recursion directory
+ */
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+
+
+#endif
