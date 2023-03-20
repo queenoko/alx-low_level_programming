9a6f175a9219b77feeddea58a1ef26e4c022d3c2
queenoko <quimaudongwo@gmail.com>
2023-03-20 08:58:19 -0700

    FILE

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,20 @@
+/*
+ * File: 2-main.c
+ * Author: Queensly Udongwo
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file it was compiled from,
+ * followed by a new line.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
