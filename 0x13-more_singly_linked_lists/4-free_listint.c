e565d6602fcdd003c1fb26784e0967d3b35362af
queenoko <quimaudongwo@gmail.com>
2023-04-03 05:24:51 -0700

    Frees Linked list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+/*
+ * File: 4-free_listint.c
+ * Author: Queensly Udongwo
+ */
+
+#include "lists.h"
+
+/**
+ * free_listint - Frees listint_t list
+ * @head: Pointer to the head of listint_t to be freed
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp - head->next;
+		free(head);
+		head = temp;
+	}
+}
