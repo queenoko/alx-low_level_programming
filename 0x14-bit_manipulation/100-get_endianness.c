94bad97c9288dd65b9bc2105d27b2fca3601a347
queenoko <quimaudongwo@gmail.com>
2023-04-10 16:26:18 -0700

    Endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,22 @@
+/*
+ * File: 100-get_endianness.c
+ * Author: Queensly Udongwo
+ */
+
+#include "main.h"
+
+/**
+ * get_endianness - Checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	int n = 1;
+	char *endian = (char *)&n;
+
+	if (*endian == 1)
+		return (1);
+
+	return (0);
+}
