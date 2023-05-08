667fd8a379dd0f5b64aeb547ff77b47b2946f1cb
queenoko <quimaudongwo@gmail.com>
2023-05-08 16:47:43 -0700

    Read Text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,25 +14,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t op, rd, wr;
 	char *buff;
 
-	if (filename == NULL)
-		return (0);
-
-	buff = malloc(sizeof(char) * letters);
-	if (buff == NULL)
-		return (0);
-
 	op = open(filename, O_RDONLY);
+	if (op == -1)
+		return (0);
+	buff = malloc(sizeof(char) * letters);
 	rd = read(op, buff, letters);
 	wr = write(STDOUT_FILENO, buff, rd);
 
-	if (op == -1 || rd == -1 || wr == -1 || wr != rd)
-	{
-		free(buff);
-		return (0);
-	}
-
 	free(buff);
-	close(0);
-
+	close(op);
 	return (wr);
 }
