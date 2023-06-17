bc0a0fb530d5c64377a727beee635da7b8bc2988
queenoko <quimaudongwo@gmail.com>
2023-06-16 20:14:31 -0700

    gen key

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - generate and prints password crackme5 executables
+ * @argc: Number of arguments given to program
+ * @argv: Array of pointers to arguments
+ *
+ * Return: 0
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	char pass[7], *codeme;
+	int lent = strlen(argv[1]), z, temp_c;
+
+	codeme = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+
+	temp_c = (len ^ 59) & 63;
+	pass[0] = codeme[temp_c];
+
+	temp_c = 0;
+	for (z = 0; z < len; z++)
+		temp_c += argv[1][z];
+	pass[1] = codeme[(temp_c ^ 79) & 63];
+
+	temp_c = 1;
+	for (z = 0; z < len; z++)
+		temp_c *= argv[1][z];
+	pass[2] = codeme[(temp_c ^ 85) & 63];
+
+	temp_c = 0;
+	for (z = 0; z < len; z++)
+	{
+		if (argv[1][z] > temp_c)
+			temp_c = argv[1][z];
+	}
+	srand(temp_c ^ 14);
+	pass[3] = codeme[rand() & 63];
+
+	temp_c = 0;
+	for (z = 0; z < len; z++)
+		temp_c += (argv[1][z] * argv[1][z]);
+	pass[4] = codeme[(temp_c ^ 239) & 63];
+
+	for (z = 0; z < argv[1][0]; z++)
+		temp_c = rand();
+	pass[5] = codeme[(temp_c ^ 229) & 63];
+
+	pass[6] = '\0';
+	printf("%s", pass);
+	return (0);
+}
