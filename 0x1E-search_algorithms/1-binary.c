ec0ef7e1d52a3a3d8e870e17c0d6e7fa3a204d66
queenoko <quimaudongwo@gmail.com>
2023-10-10 07:39:06 -0700

    binary

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,13 +1,14 @@
 #include "search_algos.h"
 
 /**
- * binary_search - This searches for value in an integer array using a binary
- * search algorithm, not guaranteed return lowest index if `value` appears
+ * binary_search - This searches for value
+ * in an integer array using a binary
+ * search algorithm, not guaranteed return
+ * lowest index if `value` appears
  * twice in `array`
  * @array: pointer to first element of array to seach
  * @size: number of elements in array
  * @value: value to search for
- *
  * Return: index containing `value`, or -1 if `value` not found or
  * `array` is NULL
  */
